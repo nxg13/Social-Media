baac2d2abb9f75778c1ea6a8b7f516b7c6ad5dc9
nxg13 <nicholas.glass@greenwichschools.org>
2017-03-16 11:13:00 -0400

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,30 @@
+//FIREBASE
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function signIn() {
+	console.log("Entered the Function")
+	
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  console.log("good?")
+	  console.log(user.displayName)
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  console.log("not good?")
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+}
+
 
 /* DROP-DOWN MENU: When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
