9cb996a1b1b55878db8225ae877635a9cbd08b9d
nxg13 <nicholas.glass@greenwichschools.org>
2017-03-17 10:10:03 -0400

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -5,7 +5,7 @@ provider.setCustomParameters({
   'login_hint': 'user@example.com'
 });
 
-function(signIn) {
+function signIn() {
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
 	  var token = result.credential.accessToken;
