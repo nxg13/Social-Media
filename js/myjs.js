351ff4b4bb80c346243b18d97b46b5342ce66205
nxg13 <nicholas.glass@greenwichschools.org>
2017-03-17 10:30:51 -0400

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -24,28 +24,6 @@ function signIn() {
 	  var credential = error.credential;
 	  // ...
 	});
-
-
-    firebase.auth().signInWithRedirect(provider);
-
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-         // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // ...
-        }
-         // The signed-in user info.
-         var user = result.user;
-        }).catch(function(error) {
-         // Handle Errors here.
-         var errorCode = error.code;
-         var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-        });
 }
 
 // DROP-DOWN MENU: When the user clicks on the button, toggle between hiding and showing the dropdown content 
